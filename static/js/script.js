b9031d0a8f4089bdbcf5b052466bb1d8888d236a
eternal-flame-AD <fu@utexas.edu>
2021-07-13 23:28:24 -0500

    sensitive image blur

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,11 @@
 $('.dropdown-trigger').dropdown();
 
-$(document).ready(function(){
-  $('.sidenav').sidenav();
+$(document).ready(function() {
+    $('.sidenav').sidenav();
 });
+
+document.querySelectorAll("div.sensitive-img-unblur").forEach(el => {
+    el.onclick = (e) => {
+        el.parentElement.classList.add("acknowledged")
+    }
+})
